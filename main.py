2fc3638ae664a8d7a6546ddcb14bcd598c1c91e9
florije1988 <florije1988@gmail.com>
2015-07-30 10:33:14 +0800

    测试dict的config形式。

--- main.py
+++ main.py
@@ -10,7 +10,51 @@ import my_module
 
 # load the logging configuration
 
-logging.config.fileConfig('logging.ini', disable_existing_loggers=False)
+# logging.config.fileConfig('logging.ini', disable_existing_loggers=False)
+
+logging_config = dict(
+    version=1,
+    disable_existing_loggers=False,
+    formatters={
+        'format': {'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'}
+    },
+    handlers={
+        'sh': {'class': 'logging.StreamHandler',
+               'formatter': 'format',
+               'level': logging.DEBUG}
+    },
+    loggers={
+        '': {'handlers': ['sh'],
+             'level': logging.DEBUG}
+    }
+)
+
+another_config = {
+    'version': 1,
+    'disable_existing_loggers': False,  # this fixes the problem
+
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'default': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default'],
+            'level': 'INFO',
+            'propagate': True
+        }
+    }
+}
+
+logging.config.dictConfig(another_config)
 logger = logging.getLogger(__name__)
 
 logger.info('start.')
